bd03ec49e5bf6fc722f13a0125eed6be9e3c83fd
gitdemo5 <vijayalaxmibajantri8@gmail.com>
2023-10-02 22:05:23 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>hello form v2</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
